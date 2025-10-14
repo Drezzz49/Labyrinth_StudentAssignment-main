1c804245d0b89f53eebdc820cc0cbd7b74285a5f
Samuel <samuel.saxgarn@gmail.com>
2025-10-14 14:50:57 +0200

    bajs algoritm
    den funkar inte

--- Assets/Student/Scripts/PathfinderAlgorithm.cs
+++ Assets/Student/Scripts/PathfinderAlgorithm.cs
@@ -31,12 +31,81 @@ public static class PathfindingAlgorithm
      
      HINT: Start simple with BFS (ignore wall costs and vents), then extend to weighted Dijkstra
      </summary> */
+
+    private static List<Vector2Int> path = new List<Vector2Int>();
+    private static Dictionary<Vector2Int, Vector2Int> previousSteps = new Dictionary<Vector2Int, Vector2Int>();
+    private static Dictionary<Vector2Int, float> distances = new Dictionary<Vector2Int, float>();
+
+
     public static List<Vector2Int> FindShortestPath(Vector2Int start, Vector2Int goal, IMapData mapData)
     {
         // TODO: Implement your pathfinding algorithm here
 
-        Debug.LogWarning("FindShortestPath not implemented yet!");
-        return null;
+        //path.Add(start + Vector2Int.down);
+        //path.Add(start + 2 * Vector2Int.down);
+
+        Vector2Int[] dirs = new Vector2Int[4] { Vector2Int.up, Vector2Int.down, Vector2Int.right, Vector2Int.left };
+        Vector2Int previousStep = start;
+
+        int maxFail = 1000;
+        int failCount = 0;
+
+        while (true)
+        {
+            Vector2Int newStep = new Vector2Int(-1, -1);
+            List<Vector2Int> possibleSteps = new List<Vector2Int>();
+
+            foreach (Vector2Int dir in dirs)
+            {
+                Vector2Int newStepTotest = previousStep + dir;
+
+                if (!CheckWall(mapData, previousStep, dir))
+                    possibleSteps.Add(newStepTotest);
+            }
+
+            if (possibleSteps.Count > 0)
+                Debug.LogError("Hjälp");
+
+            if (possibleSteps.Count > 1)
+            {
+                (Vector2Int pos, float distance) minDistance = (Vector2Int.zero, -1);
+
+                foreach (Vector2Int step in possibleSteps)
+                {
+                    if (step == previousStep && possibleSteps.Count != 3)
+                        continue;
+
+                    if (minDistance.distance == -1)
+                    {
+                        minDistance = (step, Vector2Int.Distance(step, goal));
+                        continue;
+                    }
+
+                    float checkDistance = Vector2Int.Distance(step, goal);
+                    if (checkDistance < minDistance.distance)
+                        minDistance = (step, checkDistance);
+                }
+            }
+            else
+                newStep = possibleSteps[0];
+
+            if (newStep != new Vector2Int(-1, -1))
+            {
+                path.Add(newStep);
+                failCount = 0;
+            }
+            else
+                failCount++;
+
+            if (previousStep == goal || failCount == maxFail)
+                break;
+        }
+
+        foreach (Vector2Int step in path)
+            Debug.Log(step);
+
+        //Debug.LogWarning("FindShortestPath not implemented yet!");
+        return path;
     }
 
     public static bool IsMovementBlocked(Vector2Int from, Vector2Int to, IMapData mapData)
@@ -44,5 +113,35 @@ public static class PathfindingAlgorithm
         // TODO: Implement movement blocking logic
         // For now, allow all movement so character can move while you work on pathfinding
         return false;
+    }
+
+    private static bool CheckWall(IMapData mapData, Vector2Int pos, Vector2Int dir)
+    {
+        {
+            Vector2Int toCheck = pos + dir;
+            if (toCheck.x < 0 || toCheck.y < 0 || toCheck.x >= mapData.Width || toCheck.y >= mapData.Height)
+                return true;
+        }
+
+        if (dir == Vector2Int.up)
+        {
+            Vector2Int toCheck = pos + Vector2Int.up;
+            return mapData.HasHorizontalWall(toCheck.x, toCheck.y);
+        }
+        else if (dir == Vector2Int.down)
+        {
+            Vector2Int toCheck = pos;
+            return mapData.HasHorizontalWall(toCheck.x, toCheck.y);
+        }
+        else if (dir == Vector2Int.right)
+        {
+            Vector2Int toCheck = pos + Vector2Int.right;
+            return mapData.HasVerticalWall(toCheck.x, toCheck.y);
+        }
+        else
+        {
+            Vector2Int toCheck = pos;
+            return mapData.HasVerticalWall(toCheck.x, toCheck.y);
+        }
     }
 }
