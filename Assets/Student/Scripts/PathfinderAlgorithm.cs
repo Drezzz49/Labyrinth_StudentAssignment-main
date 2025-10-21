3afd00c31b0a5f8215b3aaa5c1ec72a399faf546
Samuel <samuel.saxgarn@gmail.com>
2025-10-21 15:48:47 +0200

    A*

--- Assets/Student/Scripts/PathfinderAlgorithm.cs
+++ Assets/Student/Scripts/PathfinderAlgorithm.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -45,11 +46,11 @@ public static class PathfindingAlgorithm
         Dictionary<Vector2Int, Vector2Int> cameFrom = new Dictionary<Vector2Int, Vector2Int>();
         Vector2Int[] dirs = { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
 
-    
 
 
 
-        BFS(start, goal, mapData);
+        AStar(start, goal, mapData);
+        //BFS(start, goal, mapData);
 
         return path;
     }
@@ -59,6 +60,72 @@ public static class PathfindingAlgorithm
         // TODO: Implement movement blocking logic
         // For now, allow all movement so character can move while you work on pathfinding
         return false;
+    }
+
+    private static void AStar(Vector2Int start, Vector2Int goal, IMapData mapData)
+    {
+        Vector2Int[] dirs = { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
+        List<Vector2Int> OpenList = new List<Vector2Int> { start };
+        HashSet<Vector2Int> closedList = new HashSet<Vector2Int>();
+
+        Dictionary<Vector2Int, float> gCost = new Dictionary<Vector2Int, float> { [start]=0 }; //beskriver kostnaden att komma givna positionen från start
+        Dictionary<Vector2Int, float> hCost = new Dictionary<Vector2Int, float> { [start]=Heuristic(start, goal) }; //beskriver kostnaden för fågelvägen från givna positionen till målet
+        Dictionary<Vector2Int, Vector2Int> tempPath = new Dictionary<Vector2Int, Vector2Int>();
+
+        while (OpenList.Count > 0)
+        {
+            OpenList.OrderBy(pos => gCost[pos] + hCost[pos]); //sortera listan beroende på hur högt värde de har på gCost och hCost på pos (vi räknar ut f kostnaden g+h=f)
+            Vector2Int currentPosition = OpenList[0]; //hämtar positionen med bäst kostnad (lägst kostnad)
+
+            if (currentPosition == goal) { break; }
+
+            OpenList.Remove(currentPosition);
+            closedList.Add(currentPosition);
+
+
+            foreach (var dir in dirs)
+            {
+                var newPos = currentPosition + dir;
+                if (CheckWall(mapData, currentPosition, dir) || closedList.Contains(newPos)) //om det finns en vägg eller vi redan varit på positionen
+                {
+                    continue;
+                }
+
+                float newGScore = gCost[currentPosition] + 1;
+
+                if (!gCost.ContainsKey(newPos) || newGScore < gCost[newPos]) //vi kollar om vi har räknat ut gCost tidigare, eller om det nya gCost är bättre, då uppdaterar vi det 
+                {
+                    gCost[newPos] = newGScore;
+                    hCost[newPos] = Heuristic(newPos, goal);
+
+                    tempPath[newPos] = currentPosition;
+
+                    if (!OpenList.Contains(newPos))
+                    {
+                        OpenList.Add(newPos);
+                    }
+                }
+            }
+        }
+
+        Vector2Int tempPosition = goal;
+
+        while (tempPath.ContainsKey(tempPosition)) //bygger upp path genom att gå baklänges igenom tempPath
+        {
+            tempPosition = tempPath[tempPosition];
+            path.Add(tempPosition);
+        }
+        path.Reverse();
+        path.Add(goal);
+
+    }
+
+    private static float Heuristic(Vector2Int current, Vector2Int goal) //kostnad fågelvägen
+    {
+        var x = Mathf.Abs(current.x - goal.x);
+        var y = Mathf.Abs(current.y - goal.y);
+
+        return x + y;
     }
 
 
